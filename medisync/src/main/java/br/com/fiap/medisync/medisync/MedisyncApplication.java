760838aee401f8a3bb23fc3e6258a7e8dbff7fa9
dsouzajm <danielsouza.jm@gmail.com>
2025-04-04 21:34:41 -0300

    Configuracoes iniciais.

---
+++ medisync/src/main/java/br/com/fiap/medisync/medisync/MedisyncApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.medisync.medisync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisyncApplication.class, args);
+	}
+
+}
---
+++ medisync/src/test/java/br/com/fiap/medisync/medisync/MedisyncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.medisync.medisync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
